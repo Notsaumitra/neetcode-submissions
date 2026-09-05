60e52168c7f6db94ae38511b2d0011d7d74ff265
Notsaumitra <82860406+Notsaumitra@users.noreply.github.com>
2026-09-05 22:38:55 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    // check order, 1st should be any open and push in stack, when comparing closed check top it must include same opening one
+    isValid(s) {
+        let bracArr = [];
+        for(let i=0; i<s.length; i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                bracArr.push(s[i]);
+            }else if(i>0 && s[i]==')'){
+                const matchBrac = bracArr[bracArr.length-1];
+                if(matchBrac=='('){
+                    bracArr.pop();
+                }else{
+                    return false;
+                }
+            }else if(i>0 && s[i]=='}'){
+                const matchBrac = bracArr[bracArr.length-1];
+                if(matchBrac=='{'){
+                    bracArr.pop();
+                }else{
+                    return false;
+                }
+            }else if(i>0 && s[i]==']'){
+                const matchBrac = bracArr[bracArr.length-1];
+                if(matchBrac=='['){
+                    bracArr.pop();
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+        console.log(bracArr);
+        return bracArr.length==0;
+    }
+}
